0c17ff83872359ee48c1c94f76ca4d13ee34db65
naro36 <47540109+vakho-naro@users.noreply.github.com>
2021-12-15 18:57:30 +0400

    last changes
    last changes

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { IUsers } from '../users';
 
 @Injectable({
   providedIn: 'root'
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UsersService } from '../services/users.service';
 
